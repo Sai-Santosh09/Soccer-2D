421bbfa3609aa1e918fdccc56b236f12d3e94a2a
Sai-Santosh09 <saisantosh2712@gmail.com>
2026-03-28 21:32:43 +0530

    now the player can use the tackle(shoot) ability

--- scenes/characters/scripts/player.gd
+++ scenes/characters/scripts/player.gd
@@ -1,6 +1,9 @@
 class_name Player extends CharacterBody2D
 
+const DURATION_TACKLE := 200
+
 enum ControlScheme {CPU, P1, P2}
+enum State {MOVING, TACKLING}
 
 @export var control_scheme : ControlScheme
 @export var speed : float = 80.0
@@ -10,13 +13,23 @@ enum ControlScheme {CPU, P1, P2}
 
 
 var Left_or_Right := Vector2.RIGHT
+var state := State.MOVING
+var time_start_tackle := Time.get_ticks_msec()
 
 func _process( _delta : float ) -> void:
 	if control_scheme == ControlScheme.CPU:
 		pass
 	else:
-		handle_human_movement()
-	set_movement_animation()
+		if state == State.MOVING:
+			handle_human_movement()
+			if velocity != Vector2.ZERO and KeyUtil.is_action_just_pressed(control_scheme, KeyUtil.Action.SHOOT):
+				state = State.TACKLING
+				time_start_tackle = Time.get_ticks_msec()
+			set_movement_animation()
+		elif state == State.TACKLING:
+			animation_player.play("tackle")
+			if Time.get_ticks_msec() - time_start_tackle > DURATION_TACKLE:
+				state = State.MOVING
 	set_Left_or_Right()
 	flip_sprite()
 	move_and_slide()
